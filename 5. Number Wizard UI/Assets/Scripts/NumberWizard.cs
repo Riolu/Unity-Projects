007aa54727381f0f7ab30e1dfe228b1a4ddec411
Riolu <hankunyan68@gmail.com>
2019-07-09 23:48:43 -0700

    finish

---
+++ 5. Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max, min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        guess = (min + max) / 2;
+        guessText.text = guess.ToString();
+    }
+
+    public void onPressHigher() //guessed number is higher
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void onPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        guessText.text = guess.ToString();
+    }
+}
